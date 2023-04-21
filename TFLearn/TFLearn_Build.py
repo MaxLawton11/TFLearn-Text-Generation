9f8e19d906e65f5222d5522e8cae35674e35e161
Max Lawton <max@lawtonnation.com>
2023-04-20 20:10:53 -0400

    added data

--- TFLearn/TFLearn_Build.py
+++ TFLearn/TFLearn_Build.py
@@ -4,10 +4,12 @@ from TfLearn_Model import *
 from parentdirectory import Constants
 from Constants import *
 
-m = Model("path/to/text/file.txt")
+m = Model("testdata.txt")
 X, Y = m.preprocess_data()
 m.build_model()
 m.train_model(X, Y)
 
 # Save the trained model
 m.model.save(m.model_path)
+
+print("--Built Model--")
