e208a1336570fa83402db720b2fcf3a4e464bfa8
Max Lawton <max@lawtonnation.com>
2023-04-20 20:47:01 -0400

    changed chekpint name

--- TFLearn/Constants.py
+++ TFLearn/Constants.py
@@ -1,5 +1,5 @@
 Constant_maxlen = 25
 Constant_model_path='model.tfl'
-Constant_checkpoint_path = 'model.ckpt'
+Constant_checkpoint_path = 'checkpoints'
 Constant_epochs = 5
 Constant_run_id = 'text_generation'
