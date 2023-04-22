0149e38b9955a1e8a45a0129d21ea5914cee6641
Max Lawton <max@lawtonnation.com>
2023-04-22 18:26:13 -0400

    added new

--- Train.py
+++ Train.py
@@ -1,8 +1,15 @@
 import tensorflow as tf
 from Model import *
+import os.path
 
 #init and train
 m = Model("DataSet.text")
+
+if os.path.isfile("model_instance.tflearn.index") and  os.path.isfile("model_instance.tflearn.meta") :
+    m.model.load("model_instance.tflearn")
+else :
+    pass
+
 m.train(300)
 
 m.save("model_instance.tflearn")
