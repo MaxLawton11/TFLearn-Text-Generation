c2aa5898a41981e234f736f7959e9d686bae7e73
Max Lawton <max@lawtonnation.com>
2023-04-21 20:39:12 -0400

    changed to dill

--- TFLearn/Generate.py
+++ TFLearn/Generate.py
@@ -1,9 +1,11 @@
-import pickle
+import dill
 from Model import *
 
-with open('model.pkl', 'rb') as inp:
-    m = pickle.load(inp)
-    text = m.generate(15, "a", 0.5)
-    print("Text: ", text)
+with open('model_dill.pkl', 'rb') as inp:
+    m = dill.load(inp)
+
+m = pickle.load(inp)
+text = m.generate(15, "a", 0.5)
+print("Text: ", text)
     
 del m
--- TFLearn/Train.py
+++ TFLearn/Train.py
@@ -1,18 +1,12 @@
-import pickle
+import dill  
 from Model import *
-from multiprocessing import Lock
-
-lock = Lock()
 
 m = Model("DataSet.text")
 m.train(10)
 
-lock.acquire()
+with open('model_dill.pkl', 'wb') as outp:
+    dill.dump(m, outp)
 
-with open('model.pkl', 'wb') as outp:
-    pickle.dump(m, outp, pickle.HIGHEST_PROTOCOL)
-
-lock.release()
 
 print("--- Model Traind & Saved ---")
 del m
