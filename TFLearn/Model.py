0f7390e593741173ce56fd160e4ee99f2301e18a
Max Lawton <max@lawtonnation.com>
2023-04-21 20:07:09 -0400

    oh?

--- TFLearn/Model.py
+++ TFLearn/Model.py
@@ -8,7 +8,7 @@ class Model(object):
         self.path = path
         self.maxlen = 25
         self.char_idx = None
-        X, Y, char_idx = textfile_to_semi_redundant_sequences(self.path, seq_maxlen=self.maxlen, redun_step=1)
+        X, Y, self.char_idx = textfile_to_semi_redundant_sequences(self.path, seq_maxlen=self.maxlen, redun_step=1)
 
         # Define the network architecture
         input_layer = tflearn.input_data(shape=[None, self.maxlen, len(self.char_idx)])
