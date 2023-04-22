ad1cf5331e8757ae1dd51ec789e2bb7151749a6b
Max Lawton <max@lawtonnation.com>
2023-04-21 21:02:12 -0400

    added load

--- TFLearn/Generate.py
+++ TFLearn/Generate.py
@@ -1,13 +1,8 @@
 import tensorflow as tf
 from Model import *
 
-
-with tf.Session(graph=tf.Graph()) as sess:
-    tf.saved_model.loader.load(sess, [tf.saved_model.tag_constants.SERVING], './models')
-    
-    # get a handle to the module
-    m = tf.get_default_graph().get_tensor_by_name('m:0')
-
+m = Model("DataSet.text")
+m.model.load("my_model.tflearn")
 
 text = m.generate(15, "a", 0.5)
 print("Text: ", text)
