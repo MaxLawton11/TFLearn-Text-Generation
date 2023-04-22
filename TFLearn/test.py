f9170e77b19440fced5a772d374ffdff692f9952
Max Lawton <max@lawtonnation.com>
2023-04-21 20:20:59 -0400

    test

--- TFLearn/test.py
+++ TFLearn/test.py
@@ -3,6 +3,10 @@ from Model import *
 m = Model("DataSet.text")
 m.train(10)
 
-text = m.generate("h", 0.5)
-print(text)
+text = m.generate(5, "h", 0.5)
 
+m.train(10)
+
+text = m.generate(10, "a", 0.5)
+
+print(text)
