77f708864257c1741698f760791205c22d95275f
Max Lawton <max@lawtonnation.com>
2023-04-23 21:19:58 -0400

    new path config

--- Train.py
+++ Train.py
@@ -1,18 +1,24 @@
 import tensorflow as tf
 from Model import *
+import os
 import os.path
 
-#init and train
-m = Model("DataSet.text")
+path = "DataSets"
+dir_list = os.listdir(path)
 
-if os.path.isfile("model_instance.tflearn.index") and  os.path.isfile("model_instance.tflearn.meta") :
-    m.model.load("model_instance.tflearn")
-else :
-    pass
+for path in dir_list :
+    #create model
+    m = Model("DataSet.text")
 
-m.train(300)
+    if os.path.isfile("model_instance.tflearn.index") and  os.path.isfile("model_instance.tflearn.meta") :
+        m.model.load("model_instance.tflearn")
+    else :
+        pass
 
-m.save("model_instance.tflearn")
+    m.train(300)
+    
+    m.save("model_instance.tflearn")
+    del m
 
 print("--- Model Traind & Saved ---")
 del m
