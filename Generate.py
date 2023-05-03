b74370ac5558d9b1245d3b6736b715ea6bbe8357
Max Lawton <91839824+MaxLawton11@users.noreply.github.com>
2023-05-02 20:45:02 -0400

    Update Generate.py

--- Generate.py
+++ Generate.py
@@ -2,9 +2,6 @@ import tensorflow as tf
 from Model import *
 import sys
 
-m = Model("DataSet.text")
-m.model.load("model_instance.tflearn")
-
 if len(sys.argv) < 3 :
     sys.argv.append(25)
 elif len(sys.argv) < 2 :
@@ -21,6 +18,20 @@ except Exception :
     print("Incorrect Arguments")
     raise "./generate.sh [base_text] [n_char = 25]"
 
+    
+import os
+import os.path
+
+dir_list = os.listdir(path)
+
+print("--------- Generating ---------")
+
+if not path :
+    print("No vaild datasets")
+    quit()
+
+m = Model(path[0])
+m.model.load("model_instance.tflearn")
 text = m.generate(int(sys.argv[2]), str(sys.argv[1]), 0.5)
 print("Text: ", text)
     
