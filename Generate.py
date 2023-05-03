cbf490b5540a3bbfa05af27b3d3835269ebed187
Max Lawton <91839824+MaxLawton11@users.noreply.github.com>
2023-05-02 21:01:36 -0400

    Update Generate.py

--- Generate.py
+++ Generate.py
@@ -31,7 +31,7 @@ if not dir_list :
     print("No vaild datasets")
     quit()
 
-m = Model(dir_list[0])
+m = Model(f"{path}/{dir_list[0}")
 m.model.load("model_instance.tflearn")
 text = m.generate(int(sys.argv[2]), str(sys.argv[1]), 0.5)
 print("Text: ", text)
