d472b3702cf22999dffa4783364f47de28a01451
Max Lawton <91839824+MaxLawton11@users.noreply.github.com>
2023-05-04 10:32:45 -0400

    Update BuildDataset.py

--- Python/BuildDataset.py
+++ Python/BuildDataset.py
@@ -1,2 +1,9 @@
-print("Pre-made dataset already included")
-print("No changes made")
+import requests
+
+url = "https://raw.githubusercontent.com/tflearn/tflearn.github.io/master/resources/shakespeare_input.txt"
+response = requests.get(url)
+
+sets_path = "DataSets"
+
+with open(f"{sets_path}/shakespeare_input.txt", "w") as file:
+    file.write(response.text)
