7ef862efc1d4d710ae5150e55c0d2c2a8cd06a88
Max Lawton <91839824+MaxLawton11@users.noreply.github.com>
2023-05-04 22:15:54 -0400

    Update Train.py

--- Python/Train.py
+++ Python/Train.py
@@ -3,16 +3,18 @@ import os
 import os.path
 import sys
 
-import sys
-
-if len(sys.argv) != 2 or type(sys.argv[1]) != int or sys.argv[1] < 1:
-    print("Incorrect Argument")
-    print("See README.md for usage")
-    quit()
-
 sets_path = "DataSets"
 dir_list = os.listdir(sets_path)
 training_cycles = 1
+
+if len(sys.argv) < 2 :
+    training_cycles = 1
+elif len(sys.argv) > 2 or type(sys.argv[1]) != int or sys.argv[1] < 0:
+    print("Incorrect Argument")
+    print("See README.md for usage")
+    quit()
+else :
+    training_cycles = sys.argv[1]
 
 print("--------- Training ---------")
 path_counter = 1
