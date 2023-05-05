93a889f169c8bc530454df983ecd3be92f6167b0
Max Lawton <91839824+MaxLawton11@users.noreply.github.com>
2023-05-04 22:28:36 -0400

    Update Train.py

--- Python/Train.py
+++ Python/Train.py
@@ -7,14 +7,20 @@ sets_path = "DataSets"
 dir_list = os.listdir(sets_path)
 training_cycles = 1
 
-if len(sys.argv) < 2 :
-    training_cycles = 1
-elif len(sys.argv) > 2 or not isinstance(int(sys.argv[1]), int) or sys.argv[1] < 1:
+def do_quit() :
     print("Incorrect Argument")
     print("See README.md for usage")
     quit()
-else :
-    training_cycles = int(sys.argv[1])
+
+try :
+    if len(sys.argv) < 2 :
+        training_cycles = 1
+    elif len(sys.argv) > 2 or int(sys.argv[1]) < 1:
+        do_quit()
+    else :
+        training_cycles = int(sys.argv[1])
+except :
+    do_quit()
 
 print("--------- Training ---------")
 path_counter = 1
