7c2af7ed0eaeab6dd5cf922ec1fc695809688aef
Max Lawton <91839824+MaxLawton11@users.noreply.github.com>
2023-05-08 10:22:10 -0400

    Update Generate.py

--- Python/Generate.py
+++ Python/Generate.py
@@ -7,8 +7,8 @@ import sys
 if len(sys.argv) < 3 :
     sys.argv.append(25)
 elif len(sys.argv) < 2 :
-    print("Incorrect Arguments")
-    print("./generate.sh [base_text] [n_char = 25]")
+    print("# Incorrect Arguments")
+    print("# ./generate.sh [base_text] [n_char = 25]")
     quit()
 else :
     pass
@@ -17,8 +17,8 @@ try :
     int(sys.argv[2])
     str(sys.argv[1])
 except Exception :
-    print("Incorrect Arguments")
-    raise "./generate.sh [base_text] [n_char = 25]"
+    print("# Incorrect Arguments")
+    raise "# ./generate.sh [base_text] [n_char = 25]"
 
 
 sets_path = "DataSets"
