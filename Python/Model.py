52b3b881066b2397fce4834258f155380a304b80
Max Lawton <max@lawtonnation.com>
2023-05-13 16:19:50 -0400

    changed comments

--- Python/Model.py
+++ Python/Model.py
@@ -1,19 +1,12 @@
-import warnings
-if True :
-    warnings.filterwarnings("ignore", category=DeprecationWarning, module="tflearn")
-    
-import tflearn
-from tflearn.data_utils import *
-
 class Model:
-    def __init__(self, path: str):
-        #Load and preprocess the text data
+    def __init__(self, path: str):        
+        # load and preprocess the text data
         self.path = path
         self.maxlen = 25
         self.char_idx = None
         self.X, self.Y, self.char_idx = textfile_to_semi_redundant_sequences(self.path, seq_maxlen=self.maxlen, redun_step=1)
 
-        # Define the network architecture
+        # define the network architecture
         input_layer = tflearn.input_data(shape=[None, self.maxlen, len(self.char_idx)])
         lstm_layer = tflearn.lstm(input_layer, 256)
         output_layer = tflearn.fully_connected(lstm_layer, len(self.char_idx), activation='softmax')
@@ -26,14 +19,14 @@ class Model:
                                         checkpoint_path='model.ckpt')
         
     def train(self, n_epoch: int) :
-        # Train the model
+        # train the model
         self.model.fit(self.X, self.Y, validation_set=0.1, batch_size=128,
                 n_epoch=n_epoch, run_id='text_generation')
         
     def generate(self, length: int, seed: str, temperature=0.5) :
-        #generate text
+        # generate text
         return self.model.generate(length, temperature=temperature, seq_seed=seed)
     
     def save(self, filename: str):
-        # Save the trained model to a file
+        # save the trained model to a file
         self.model.save(filename)
