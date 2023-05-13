df6e883b356e9c4a3c81e44d67f2605ee6ed49e6
Max Lawton <max@lawtonnation.com>
2023-05-13 17:04:38 -0400

    add more ui

--- Python/BuildDataset.py
+++ Python/BuildDataset.py
@@ -1,9 +1,16 @@
 import requests
 
+print("--------- Building Dataset ---------")
+
 url = "https://raw.githubusercontent.com/tflearn/tflearn.github.io/master/resources/shakespeare_input.txt"
 response = requests.get(url)
 
 sets_path = "DataSets"
+output_file_name = "shakespeare_input.txt"
 
-with open(f"{sets_path}/shakespeare_input.txt", "w") as file:
+print("# Creating file: {output_file_name}")
+
+with open(f"{sets_path}/{output_file_name}", "w") as file:
     file.write(response.text)
+
+print("--------- Success ---------")
