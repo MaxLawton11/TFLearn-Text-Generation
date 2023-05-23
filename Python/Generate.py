4d5990c82029ccc039dbe24bd1e26bf4b63a7cfa
Max Lawton <max@lawtonnation.com>
2023-05-23 11:32:56 -0400

    im so confused

--- Python/Generate.py
+++ Python/Generate.py
@@ -13,7 +13,7 @@ else :
         print('# Incorrect Argument')
         print('# See README.md for usage')
 
-seed = "brotherhood in thee no sharper spur"
+seed = "brotherhood in thee"
 temp = 0.5
 
 # file to run model
