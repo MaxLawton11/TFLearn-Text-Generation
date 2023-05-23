7f376e7f15435d83dab6e1871c9c365cf0a619e6
Max Lawton <max@lawtonnation.com>
2023-05-23 14:06:13 -0400

    removed user input

--- Python/Generate.py
+++ Python/Generate.py
@@ -3,22 +3,9 @@ import os
 import os.path
 import sys
 
-# get the number of chars we want made
-if len(sys.argv) < 2 :
-    sys.argv.append(50)
-else :
-    try :
-        int(sys.argv[1])
-        if int(sys.argv[1]) < 50 :
-            sys.argv[1] = 50
-    except Exception :
-        print('# Incorrect Argument')
-        print('# See README.md for usage')
-#      "brotherhood in thee no sharper spur"
 seed = "Ay, this is he that took King Henry's chair,"
-seed = "Ay, this is he that took "
 temp = 0.5
-n_chars = 50
+n_chars = 100
 
 # file to run model
 sets_path = 'DataSets'
@@ -34,7 +21,6 @@ m = Model(f'{sets_path}/{dir_list[0]}')
 m.model.load('model_instance.tflearn')
 
 # make text from seed
-print(m.char_idx)
 text = m.generate(n_chars, seed, temp)
 print(f'Seed: "{seed}"')
 print(f'n_chars: "{n_chars}"')
