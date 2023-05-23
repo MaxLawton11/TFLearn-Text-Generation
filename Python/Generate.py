79e4c033d02fa66e976b48b0bd858d1354f15ac3
Max Lawton <max@lawtonnation.com>
2023-05-23 12:05:47 -0400

    chaned vars

--- Python/Generate.py
+++ Python/Generate.py
@@ -8,15 +8,16 @@ if len(sys.argv) < 2 :
     sys.argv.append(50)
 else :
     try :
-        n_chars = int(sys.argv[1])
-        if n_chars < 50 :
-            n_chars = 50
+        int(sys.argv[1])
+        if int(sys.argv[1]) < 50 :
+            sys.argv[1] = 50
     except Exception :
         print('# Incorrect Argument')
         print('# See README.md for usage')
 #      "brotherhood in thee no sharper spur"
 seed = "Ay, this is he that took King Henry's chair,"
 temp = 0.5
+n_chars = int(sys.argv[1])
 
 # file to run model
 sets_path = 'DataSets'
@@ -32,7 +33,6 @@ m = Model(f'{sets_path}/{dir_list[0]}')
 m.model.load('model_instance.tflearn')
 
 # make text from seed
-#text = m.generate(int(sys.argv[1]), seed, temp)
 print(m.char_idx)
 text = m.generate(n_chars, seed, temp)
 print(f'Seed: "{seed}"')
