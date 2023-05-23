d677a675b17dd00f4f8a7891ae2dedab1740fe5d
Max Lawton <max@lawtonnation.com>
2023-05-23 14:10:15 -0400

    changed to 1000

--- Python/Generate.py
+++ Python/Generate.py
@@ -1,11 +1,10 @@
 from Model import *
 import os
 import os.path
-import sys
 
 seed = "this is he that took King"
 temp = 0.5
-n_chars = 100
+n_chars = 1000
 
 # file to run model
 sets_path = 'DataSets'
